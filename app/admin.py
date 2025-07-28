99e643329b6dec07da6e7bddfcf401cca1fe72de
向南 <xiangnan@xiangnandeMac-mini.local>
2025-07-28 22:17:21 +0800

    88

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header = '停车场后台'  # 设置header
-admin.site.site_title = '停车场后台'   # 设置title
-admin.site.index_title = '停车场后台'
